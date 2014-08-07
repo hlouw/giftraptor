4c02ce0565445ac2cc5e8f09e976d2c9964a635b
Hentie Louw <hentie.louw@gmail.com>
2014-08-07 23:32:10 +0100

    Tiny tweaks
    All part of the learning process.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,18 +3,25 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import scala.concurrent.Promise
+import scala.util.Success
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 object Application extends Controller {
 
+  val p = Promise[String]()
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
-  def profile = Action {
-    Ok(views.html.profile("hlouw"))
+  def profile(user: String) = Action {
+    Ok(views.html.profile(user))
   }
 
   def admin = Action {
-    Ok(views.html.admin("hlouw"))
+    Ok(views.html.admin(s"Administrator"))
   }
 
 }
