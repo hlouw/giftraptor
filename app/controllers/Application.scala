03561876710dc020c3a5ef49af0b897c27029ea6
Hentie Louw <hentie.louw@gmail.com>
2014-08-09 14:53:37 +0100

    Config tweaks, based on activator template
    play-mongo-knockout

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,25 +3,18 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-import scala.concurrent.Promise
-import scala.util.Success
-
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
 object Application extends Controller {
-
-  val p = Promise[String]()
 
   def index = Action {
     Ok(views.html.index())
   }
 
   def profile(user: String) = Action {
-    Ok(views.html.profile(user))
+    Ok(views.html.profile())
   }
 
   def admin = Action {
-    Ok(views.html.admin(s"Administrator"))
+    Ok(views.html.admin())
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -7,10 +7,10 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.2"
 
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache,
-  ws,
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
   "org.webjars" %% "webjars-play" % "2.3.0",
-  "org.webjars" % "bootstrap" % "3.2.0"
+  "org.webjars" % "bootstrap" % "3.2.0",
+  "org.webjars" % "knockout" % "3.1.0",
+  "org.webjars" % "requirejs" % "2.1.14-1"
+
 )
